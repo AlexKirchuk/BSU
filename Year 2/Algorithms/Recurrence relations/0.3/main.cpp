7161effd5f0c4377a3110738172e177c15230e96
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-09-22 22:29:43 +0300

    0.3 alg/RL final

---
+++ Year 2/Algorithms/Recurrence relations/0.3/main.cpp
@@ -0,0 +1,52 @@
+/*
+Дано число N. Необходимо определить,
+сколько есть бинарных строк длины N,
+в которых ровно K единиц.
+
+Input
+Первая строка входных данных содержит
+два целых неотрицательных числа N
+и K (0 <= K <= N <= 1000000).
+Output
+
+Выведите одно число - ответ на задачу.
+Так как ответ может быть очень большим,
+необходимо его вывести по модулю 10^9 + 7.
+*/
+
+#include <iostream>
+#include <vector>
+
+const long long MOD = 1000000007;
+
+long long myBinaryPow(long long a, long long p)
+{
+    long long res = 1;
+    while (p > 0)
+    {
+        if (p & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        p >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    long long n, k;
+    std::cin >> n >> k;
+
+    std::vector<long long> fact(n + 1), invfact(n + 1);
+    fact[0] = 1;
+    for (int i = 1; i <= n; i++) fact[i] = (fact[i - 1] * i) % MOD;
+
+    invfact[n] = myBinaryPow(fact[n], MOD - 2);
+    for (int i = n - 1; i >= 0; i--) invfact[i] = (invfact[i + 1] * (i + 1)) % MOD;
+
+    long long res = fact[n];
+    res = (res * invfact[k]) % MOD;
+    res = (res * invfact[n - k]) % MOD;
+
+    std::cout << res << std::endl;
+    return 0;
+}
