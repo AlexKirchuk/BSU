7af2d5c7bf0d7b432b816b258e5ff98d0f9b1815
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-10-02 10:34:55 +0300

    lab4 ind java with assert

--- Year 2/Industrial Programming/Lab4 Individual/src/Main.java
+++ Year 2/Industrial Programming/Lab4 Individual/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         Interval a = new Interval(1, 5);
         Interval b = new Interval(3, 7);
 
+        assert a.length() == 4 : "Length of A should be 4";
+        assert b.length() == 4 : "Length of B should be 4";
+
         System.out.println("Interval A: " + a);
         System.out.println("Interval B: " + b);
 
@@ -11,9 +14,11 @@ public class Main {
 
         a.shift(2);
         System.out.println("A after shifting: " + a);
+        assert a.toString().equals("(3.0, 7.0)") : "After shift(2), A should be (3,7)";
 
         b.scale(0.5);
         System.out.println("B after scaling: " + b);
+        assert Math.abs(b.length() - 2) < 1e-9 : "After scaling by 0.5, B's length should be 2";
 
         System.out.println("Comparison of A and B (by length): " + a.compareTo(b));
         if (a.compareTo(b) > 0)
@@ -21,11 +26,16 @@ public class Main {
         else
             System.out.println("B is bigger than A");
 
-        System.out.println("Union (sum) of A and B: " + a.sum(b));
+        Interval union = a.sum(b);
+        System.out.println("Union (sum) of A and B: " + union);
+        assert union.toString().equals("(3.0, 7.0)") : "Union should be (3,7)";
 
         System.out.print("Difference A - B: ");
-        for (Interval d : a.difference(b)) {
+        Interval[] diff = a.difference(b);
+        for (Interval d : diff) {
             System.out.print(d + " ");
         }
+        System.out.println();
+        assert diff.length == 2 : "Difference should produce 2 intervals";
     }
 }
