e75588e8ae01a8c80c5521805039f2ae4aa224eb
Alexander <a.kirtchouk@gmail.com>
2025-10-10 22:28:53 +0300

    Delete Year 2/Operating Systems/Lab2/Lab2 CMake directory

--- Year 2/Operating Systems/Lab2/Lab2 CMake/include/average.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <data.h>
-#include <windows.h>
-
-DWORD WINAPI average_thread(LPVOID param);
--- Year 2/Operating Systems/Lab2/Lab2 CMake/include/minmax.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <data.h>
-#include <windows.h>
-
-DWORD WINAPI min_max_thread(LPVOID param);
