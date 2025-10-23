7dc14789f95f3c8d7fe1a2b118eb9716052cdbe7
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-10-23 10:07:08 +0300

    lab6-7 java precious and semiprecious

---
+++ Year 2/Industrial Programming/Lab6-7/src/PreciousStone.java
@@ -0,0 +1,5 @@
+public class PreciousStone extends Stone {
+    public PreciousStone(String name, double weight, double pricePerCarat, double transparency) {
+        super(name, weight, pricePerCarat, transparency);
+    }
+}
