89cb697b936364ede56577edfacdacd7f1651f18
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-10-24 22:43:30 +0300

    Opt Task #1 vector 60/100

--- Year 2/Algorithms/Optional Tasks/Task #1/main.cpp
+++ Year 2/Algorithms/Optional Tasks/Task #1/main.cpp
@@ -21,48 +21,14 @@ Output
 #include <string>
 #include <vector>
 
-long long myMax(long long a, long long b)
+std::string myMin(std::string a, std::string b)
 {
-    return a > b ? a : b;
+    return a < b ? a : b;
 }
 
-std::string build(const std::string& s, const std::vector<std::vector<int>>& f, bool maximaze)
+std::string myMax(std::string a, std::string b)
 {
-    int n = s.size();
-    int i = 0, j = n - 1;
-    std::string left = "", right = "";
-
-    while (i <= j) {
-        if (s[i] == s[j])
-        {
-            left += s[i];
-            if (i != j) right = s[j] + right;
-            ++i;
-            --j;
-        }
-        else if (i + 1 < n && f[i + 1][j] > f[i][j - 1])
-        {
-            ++i;
-        }
-        else if (i + 1 < n && f[i + 1][j] < f[i][j - 1])
-        {
-            --j;
-        }
-        else
-        {
-            if (maximaze)
-            {
-                if (s[i] <= s[j]) ++i;
-                else --j;
-            }
-            else
-            {
-                if (s[i] >= s[j]) ++i;
-                else --j;
-            }
-        }
-    }
-    return left + right;
+    return a > b ? a : b;
 }
 
 int main()
@@ -72,27 +38,54 @@ int main()
     int n = s.size();
 
     std::vector<std::vector<int>> f(n, std::vector<int>(n, 0));
-    for (int i = 0; i < n; ++i) f[i][i] = 1;
+    std::vector<std::vector<std::string>> minp(n, std::vector<std::string>(n, ""));
+    std::vector<std::vector<std::string>> maxp(n, std::vector<std::string>(n, ""));
+
+    for (int i = 0; i < n; ++i)
+    {
+        f[i][i] = 1;
+        minp[i][i] = maxp[i][i] = std::string(1, s[i]);
+    }
 
     for (int d = 1; d < n; ++d)
     {
-        for (int i = 0; i < n - d; ++i)
+        for (int i = 0; i + d < n; ++i)
         {
             int j = i + d;
             if (s[i] == s[j])
             {
                 f[i][j] = f[i + 1][j - 1] + 2;
+                std::string mid_min = (i + 1 <= j - 1) ? minp[i + 1][j - 1] : "";
+                std::string mid_max = (i + 1 <= j - 1) ? maxp[i + 1][j - 1] : "";
+                minp[i][j] = s[i] + mid_min + s[j];
+                maxp[i][j] = s[i] + mid_max + s[j];
             }
             else
             {
-                f[i][j] = myMax(f[i + 1][j], f[i][j - 1]);
+                if (f[i + 1][j] > f[i][j - 1])
+                {
+                    f[i][j] = f[i + 1][j];
+                    minp[i][j] = minp[i + 1][j];
+                    maxp[i][j] = maxp[i + 1][j];
+                }
+                else if (f[i + 1][j] < f[i][j - 1])
+                {
+                    f[i][j] = f[i][j - 1];
+                    minp[i][j] = minp[i][j - 1];
+                    maxp[i][j] = maxp[i][j - 1];
+                }
+                else
+                {
+                    f[i][j] = f[i + 1][j];
+                    minp[i][j] = myMin(minp[i + 1][j], minp[i][j - 1]);
+                    maxp[i][j] = myMax(maxp[i + 1][j], maxp[i][j - 1]);
+                }
             }
         }
     }
 
-    std::string min_pal = build(s, f, false);
-    std::string max_pal = build(s, f, true);
+    std::cout << minp[0][n - 1] << "\n";
+    std::cout << maxp[0][n - 1] << "\n";
 
-    std::cout << min_pal << "\n" << max_pal << "\n";
     return 0;
 }
