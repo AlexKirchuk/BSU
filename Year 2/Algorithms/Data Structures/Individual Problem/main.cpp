2dfd12bcc43fe0cd835158450d2d7c319ab1bbc6
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-11-25 21:40:41 +0300

    ind DSs O(n^2 log n)

--- Year 2/Algorithms/Data Structures/Individual Problem/main.cpp
+++ Year 2/Algorithms/Data Structures/Individual Problem/main.cpp
@@ -1,99 +1,78 @@
 #include <fstream>
-#include <set>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 int main()
 {
     std::ifstream fin("input.txt");
     std::ofstream fout("output.txt");
 
-    int n, w, h, maxArea = 0;
+    long long n, w, h;
     fin >> n >> w >> h;
 
-    std::set<int> xs, ys;
-    std::vector<std::pair<int, int>> trees(n);
-    xs.insert(0);
-    xs.insert(w);
-    ys.insert(0);
-    ys.insert(h);
-
-    for (int i = 0; i < n; i++)
+    std::vector<std::pair<long long, long long>> trees(n);
+    for (long long i = 0; i < n; i++)
     {
         fin >> trees[i].first >> trees[i].second;
-        xs.insert(trees[i].first);
-        ys.insert(trees[i].second);
     }
 
-    std::vector<int> cx(xs.begin(), xs.end());
-    std::vector<int> cy(ys.begin(), ys.end());
+    trees.push_back({ 0, -1 });
+    trees.push_back({ w, -1 });
 
-    auto getX = [&](int x)
-        {
-            return std::lower_bound(cx.begin(), cx.end(), x) - cx.begin();
-        };
-    auto getY = [&](int y)
-        {
-            return std::lower_bound(cy.begin(), cy.end(), y) - cy.begin();
-        };
+    std::sort(trees.begin(), trees.end());
+    n = trees.size();
 
-    for (auto& t : trees)
+    long long maxArea = 0;
+
+    for (long long i = 0; i < n; i++)
     {
-        t.first = getX(t.first);
-        t.second = getY(t.second);
-    }
+        std::set<long long> ys;
+        ys.insert(0);
+        ys.insert(h);
 
-    int X = cx.size(), Y = cy.size();
-    std::vector<std::vector<int>> grid(X, std::vector<int>(Y, 0));
-    for (auto& t : trees)
-    {
-        grid[t.first][t.second] = 1;
-    }
+        std::multiset<long long> gaps;
+        gaps.insert(h);
 
-    std::vector<std::vector<int>> pref(X + 1, std::vector<int>(Y + 1, 0));
-    for (int i = 1; i <= X; i++)
-    {
-        for (int j = 1; j <= Y; j++)
-        {
-            pref[i][j] = grid[i - 1][j - 1] + pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1];
-        }
-    }
-
-    for (int y1 = 0; y1 < Y; ++y1)
-    {
-        for (int y2 = y1 + 1; y2 < Y; ++y2)
-        {
-            std::vector<int> nonEmpty(X, 0);
-            for (int x = 0; x < X; ++x)
+        auto addY = [&](long long y)
             {
-                int cnt = pref[x + 1][y2] - pref[x][y2] - pref[x + 1][y1 + 1] + pref[x][y1 + 1];
-                if (cnt > 0)
-                {
-                    nonEmpty[x] = 1;
-                }
-                else
-                {
-                    nonEmpty[x] = 0;
-                }
+                if ((ys.find(y) != ys.end())) { return; }
+
+                auto itR = ys.lower_bound(y);
+                auto itL = itR;
+                if (itL == ys.begin()) { ys.insert(y); return; }
+                --itL;
+
+                long long left = *itL;
+                long long right = *itR;
+
+                gaps.erase(gaps.find(right - left));
+                gaps.insert(y - left);
+                gaps.insert(right - y);
+
+                ys.insert(y);
+            };
+
+        for (long long j = i + 1; j < n; j++)
+        {
+            long long width = trees[j].first - trees[i].first;
+            long long height = *(gaps.rbegin());
+            long long area = width * height;
+
+            if (area > maxArea)
+            {
+                maxArea = area;
             }
-            int a = 0, non = 0;
-            for (int b = 1; b < X; ++b)
+
+            if (trees[j].second != -1)
             {
-                non += nonEmpty[b - 1];
-                while (a + 1 < b && non > 0)
-                {
-                    non -= nonEmpty[a + 1];
-                    ++a;
-                }
-                int width = cx[b] - cx[a];
-                int height = cy[y2] - cy[y1];
-                int area = width * height;
-                if (area > maxArea) maxArea = area;
+                addY(trees[j].second);
             }
         }
     }
 
     fout << maxArea;
+
     fin.close();
     fout.close();
     return 0;
