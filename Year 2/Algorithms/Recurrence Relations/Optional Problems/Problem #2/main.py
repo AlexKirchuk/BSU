5bb686423d052d6bbf58c7284bd1db5577e3904c
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-12-06 15:32:26 +0300

    patterns python optimized

--- Year 2/Algorithms/Recurrence Relations/Optional Problems/Problem #2/main.py
+++ Year 2/Algorithms/Recurrence Relations/Optional Problems/Problem #2/main.py
@@ -7,30 +7,33 @@ if m > n:
 
 S = 1 << m
 
-ok = [[True] * S for _ in range(S)]
+nexts = [[] for _ in range(S)]
 
 for a in range(S):
     for b in range(S):
         good = True
+        aa = a
+        bb = b
         for i in range(m - 1):
-            a0 = (a >> i) & 1
-            a1 = (a >> (i + 1)) & 1
-            b0 = (b >> i) & 1
-            b1 = (b >> (i + 1)) & 1
+            a0 = (aa >> i) & 1
+            a1 = (aa >> (i + 1)) & 1
+            b0 = (bb >> i) & 1
+            b1 = (bb >> (i + 1)) & 1
             if a0 == a1 == b0 == b1:
                 good = False
                 break
-        ok[a][b] = good
+        if good:
+            nexts[a].append(b)
 
 dp = [1] * S
 
 for _ in range(1, n):
     ndp = [0] * S
     for a in range(S):
-        if dp[a] != 0:
-            for b in range(S):
-                if ok[a][b]:
-                    ndp[b] += dp[a]
+        da = dp[a]
+        if da:
+            for b in nexts[a]:
+                ndp[b] += da
     dp = ndp
 
 ans = sum(dp)
