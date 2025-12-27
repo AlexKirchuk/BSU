c7e69a8b3a0e1f52d318aa219733634165318757
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-12-27 15:22:47 +0300

    schemas.py

---
+++ Year 2/Operating Systems/Colloquium2/backend/src/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Literal, Optional
+
+class TaskBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    status: Literal["todo", "in_progress", "done"]
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[Literal["todo", "in_progress", "done"]] = None
+
+class TaskResponse(TaskBase):
+    id: int
