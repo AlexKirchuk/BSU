d8f62bb4643773deabb3d51befbd37301ec6f021
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-12-31 15:15:09 +0300

    tasks.py upd

--- Year 2/Operating Systems/Colloquium2/backend/src/tasks.py
+++ Year 2/Operating Systems/Colloquium2/backend/src/tasks.py
@@ -3,7 +3,9 @@ import time
 
 
 @celery_app.task
-def task_created(task_id: int):
+def some_celery_task(task_id: int):
     time.sleep(3)
-
     print(f"Task {task_id} processed in background")
+
+
+task_created = some_celery_task
