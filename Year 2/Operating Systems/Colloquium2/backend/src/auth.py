a425d65bd56ada7a989ce7070e25eab6df972d71
Alex Kirchuk <a.kirtchouk@gmail.com>
2025-12-31 15:14:33 +0300

    auth.py upd

--- Year 2/Operating Systems/Colloquium2/backend/src/auth.py
+++ Year 2/Operating Systems/Colloquium2/backend/src/auth.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 from typing import Optional
-from jose import jwt, JWTError
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError, ExpiredSignatureError
 
 SECRET_KEY = "CHANGE_ME"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+security = HTTPBearer()
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -17,5 +20,18 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
 def decode_token(token: str):
     try:
         return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except ExpiredSignatureError:
+        return None
     except JWTError:
         return None
+
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    token = credentials.credentials
+    payload = decode_token(token)
+    if payload is None:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
+    return payload
