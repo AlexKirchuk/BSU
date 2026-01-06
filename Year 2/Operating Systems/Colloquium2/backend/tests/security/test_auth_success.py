fe4bf2440fb24cf1e8f7200cd5fcea7b118dbd1b
Alex Kirchuk <a.kirtchouk@gmail.com>
2026-01-06 19:38:25 +0300

    test_auth_success.py upd

--- Year 2/Operating Systems/Colloquium2/backend/tests/security/test_auth_success.py
+++ Year 2/Operating Systems/Colloquium2/backend/tests/security/test_auth_success.py
@@ -1,8 +1,20 @@
 from auth import create_access_token
 
 
-def test_valid_token(client):
-    token = create_access_token({"sub": "test"})
+def test_valid_token(client, db_session):
+    r = client.post("/register", json={
+        "username": "testuser",
+        "password": "secret"
+    })
+    assert r.status_code == 200
+
+    r = client.post("/login", data={
+        "username": "testuser",
+        "password": "secret"
+    })
+    assert r.status_code == 200
+    token = r.json()["access_token"]
+
     r = client.get(
         "/tasks",
         headers={"Authorization": f"Bearer {token}"}
