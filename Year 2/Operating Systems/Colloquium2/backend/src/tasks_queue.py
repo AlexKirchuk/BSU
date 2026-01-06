d1f7f4d4c0d6213ff9e5fe3d61b16e51f18d8892
Alex Kirchuk <a.kirtchouk@gmail.com>
2026-01-06 20:24:52 +0300

    tasks_queue.py upd

--- Year 2/Operating Systems/Colloquium2/backend/src/tasks_queue.py
+++ Year 2/Operating Systems/Colloquium2/backend/src/tasks_queue.py
@@ -1,9 +1,12 @@
+import os
 from celery import Celery
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 
 celery_app = Celery(
     "tasks",
-    broker="redis://localhost:6379/1",
-    backend="redis://localhost:6379/2",
+    broker=f"redis://{REDIS_HOST}:6379/1",
+    backend=f"redis://{REDIS_HOST}:6379/2",
 )
 
 celery_app.conf.task_serializer = "json"
