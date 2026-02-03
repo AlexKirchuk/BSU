ec82cb508ec8e6d1a2e663c394ed8985bf3c4695
Alex Kirchuk <a.kirtchouk@gmail.com>
2026-02-03 22:32:56 +0300

    fix

--- Year 1/Machine-Oriented Programming/Lab2/main.cpp
+++ Year 1/Machine-Oriented Programming/Lab2/main.cpp
@@ -44,49 +44,49 @@ int main()
 
 	__asm
 	{
-		mov al, '+';
-		cmp operation, al;
-		je a1;
-		jne a2;
-	a1: mov ecx, x;
-		add ecx, y;
-		mov asmResult, ecx;
-		jmp end1;
-	a2: mov al, '-';
-		cmp operation, al;
-		je a3;
-		jne a4;
-	a3: mov ecx, x;
-		sub ecx, y;
-		mov asmResult, ecx;
-		jmp end1;
-	a4: mov al, '*';
-		cmp operation, al;
-		je a5;
-		jne a6;
-	a5: mov ecx, x;
-		mov ebx, y;
-		imul ecx, ebx;
-		mov asmResult, ecx;
-		jmp end1;
-	a6: mov al, '/';
-		cmp operation, al;
-		je a7;
-		jne els;
-	a7: mov eax, x;
-		mov ecx, y;
-		cdq;
-		idiv ecx;
-		mov asmResult, eax;
-		jmp end1;
-	els: xor ecx, ecx;
-		mov asmResult, ecx;
-		jmp end1;
+		mov al, '+'
+		cmp operation, al
+		je a1
+		jne a2
+	a1: mov ecx, x
+		add ecx, y
+		mov asmResult, ecx
+		jmp end1
+	a2: mov al, '-'
+		cmp operation, al
+		je a3
+		jne a4
+	a3: mov ecx, x
+		sub ecx, y
+		mov asmResult, ecx
+		jmp end1
+	a4: mov al, '*'
+		cmp operation, al
+		je a5
+		jne a6
+	a5: mov ecx, x
+		mov ebx, y
+		imul ecx, ebx
+		mov asmResult, ecx
+		jmp end1
+	a6: mov al, '/'
+		cmp operation, al
+		je a7
+		jne els
+	a7: mov eax, x
+		mov ecx, y
+		cdq
+		idiv ecx
+		mov asmResult, eax
+		jmp end1
+	els: xor ecx, ecx
+		mov asmResult, ecx
+		jmp end1
 	end1:
-		mov eax, asmResult;
-		push eax;
-		call printAsmResult;
-		pop eax;
+		mov eax, asmResult
+		push eax
+		call printAsmResult
+		pop eax
 	}
 
 	return 0;
